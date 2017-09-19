11957fbe80ef8cf61aa683363a1ca0c5c121672d
juanfresia <juanmanuelfresia@gmail.com>
2017-09-19 17:16:42 -0300

    Primer algoritmo naive
    Signed-off-by: juanfresia <juanmanuelfresia@gmail.com>

---
+++ naive.py
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+def test_string_matching():
+		#0         1         2         3
+		#012345678901234567890123456789012345678
+	T = "HOLA ESTO ES UN STRING DE PRUEBA. HOLA."
+	P1 = "ES"
+	P2 = "HOLA"
+
+	print(string_matching_naive(T, P1))
+	print(string_matching_naive(T, P2))
+
+
+
+def string_matching_naive(texto, patron):
+	"""Devuelve una lista con las posiciones en texto
+	donde aparece patron completo."""
+
+	matches = []
+
+	for inicio in range(len(texto) - len(patron)):
+		match = True
+		for act in range(len(patron)):
+			if texto[inicio + act] != patron[act]:
+				match = False
+				break
+		if match:
+			matches.append(inicio)
+
+	return matches
+
+
+test_string_matching()
