61866cbba8bd32a6ec783dd7f9cae56c15951073
Xero-Hige <gaston.martinez.90@gmail.com>
2017-09-30 14:15:20 -0300

    Changed karpRabin module to enable silent import

--- karpRabin.py
+++ karpRabin.py
@@ -1,12 +1,13 @@
 BASE = 101
 
+
 def test_string_matching():
-        #0         1         2         3
-        #012345678901234567890123456789012345678
+    # 0         1         2         3
+    # 012345678901234567890123456789012345678
     T = "HOLA ESTO ES UN STRING DE PRUEBA. HOLA."
     P1 = "ES"
     P2 = "HOLA"
-         #012345
+    # 012345
     T2 = "AAZAAA"
     P3 = "AA"
     T3 = "ABABZABABAB"
@@ -17,13 +18,14 @@ def test_string_matching():
     print(karpRabin(T2, P3))
     print(karpRabin(T3, P4))
 
+
 def hash(texto, ini, fin, hash_ant):
     if (ini == 0):
         h = 0
         for x in range(fin):
-            h += ord(texto[x]) * BASE**(fin-1-x)
+            h += ord(texto[x]) * BASE ** (fin - 1 - x)
         return h
-    return (BASE * (hash_ant - (ord(texto[ini-1]) * BASE**(fin-ini-1))) + ord(texto[fin-1]))
+    return (BASE * (hash_ant - (ord(texto[ini - 1]) * BASE ** (fin - ini - 1))) + ord(texto[fin - 1]))
 
 
 def karpRabin(texto, patron):
@@ -31,12 +33,13 @@ def karpRabin(texto, patron):
     hash_patron = hash(patron, 0, len(patron), 0)
     hash_tent = 0
 
-    for x in range(len(texto)-(len(patron)-1)):
-        hash_tent = hash(texto, x, x+len(patron), hash_tent)
-        if ((hash_tent == hash_patron) and (patron == texto[x:x+len(patron)])):
+    for x in range(len(texto) - (len(patron) - 1)):
+        hash_tent = hash(texto, x, x + len(patron), hash_tent)
+        if ((hash_tent == hash_patron) and (patron == texto[x:x + len(patron)])):
             matches.append(x)
 
     return matches
 
 
-test_string_matching()
+if __name__ == '__main__':
+    test_string_matching()
