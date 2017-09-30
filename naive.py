72ed1d395de41629f1d2833b7c6b547cd0510fc3
Xero-Hige <gaston.martinez.90@gmail.com>
2017-09-30 14:14:19 -0300

    Changed naive module to enable silent import

--- naive.py
+++ naive.py
@@ -1,37 +1,31 @@
-
-
-
-
-
-
 def test_string_matching():
-		#0         1         2         3
-		#012345678901234567890123456789012345678
-	T = "HOLA ESTO ES UN STRING DE PRUEBA. HOLA."
-	P1 = "ES"
-	P2 = "HOLA"
+    # 0         1         2         3
+    # 012345678901234567890123456789012345678
+    T = "HOLA ESTO ES UN STRING DE PRUEBA. HOLA."
+    P1 = "ES"
+    P2 = "HOLA"
 
-	print(string_matching_naive(T, P1))
-	print(string_matching_naive(T, P2))
-
+    print(string_matching_naive(T, P1))
+    print(string_matching_naive(T, P2))
 
 
 def string_matching_naive(texto, patron):
-	"""Devuelve una lista con las posiciones en texto
-	donde aparece patron completo utilizando el algoritmo naive."""
+    """Devuelve una lista con las posiciones en texto
+    donde aparece patron completo utilizando el algoritmo naive."""
 
-	matches = []
+    matches = []
 
-	for inicio in range(len(texto) - len(patron)):
-		match = True
-		for act in range(len(patron)):
-			if texto[inicio + act] != patron[act]:
-				match = False
-				break
-		if match:
-			matches.append(inicio)
+    for inicio in range(len(texto) - len(patron)):
+        match = True
+        for act in range(len(patron)):
+            if texto[inicio + act] != patron[act]:
+                match = False
+                break
+        if match:
+            matches.append(inicio)
 
-	return matches
+    return matches
 
 
-test_string_matching()
+if __name__ == '__main__':
+    test_string_matching()
