b50909c8fa0ddbc2f907968d8c9c29d6ed56e4e7
Xero-Hige <gaston.martinez.90@gmail.com>
2017-09-30 15:07:24 -0300

    Fixed output bug

--- test_runner.py
+++ test_runner.py
@@ -28,7 +28,34 @@ Hay 3 posibles candidatos de n-1 elementos, con n={} el largo del patron.
                 """.format(self._iterations, len(self._string), len(self._pattern))
 
 
-ENABLED_TESTS = [TestNM1]
+class TestNM2(Test):
+    _string = "c" * 499 + "b" * 50 + "c" * 499 + "b" + "c" * 499
+    _pattern = "a" * 500
+    _iterations = 1000
+
+    def run(self, f):
+        for _ in range(self._iterations - 1):
+            f(self._string, self._pattern)
+        return f(self._string, self._pattern)
+
+    def get_expected_result(self):
+        return []
+
+    def get_test_name(self):
+        return "NM-2"
+
+    def get_long_test_name(self):
+        return "No Matches Test 2"
+
+    def get_test_description(self):
+        return """
+Corre {} veces el string matcher sobre una cadena de largo {} en la cual no hay matches.
+
+No hay ningun posible candidato porque no hay caracteres en comun entre el patron y el string. Patron de largo n={}.
+                """.format(self._iterations, len(self._string), len(self._pattern))
+
+
+ENABLED_TESTS = [TestNM1, TestNM2]
 TEST_RESULTS = "test_results"
 
 
@@ -44,8 +71,7 @@ def main():
         for test_class in ENABLED_TESTS:
             output.write(test_class().run_all() + "\n")
 
-            output.write("# Descriptions \n\n")
-
+        output.write("# Descriptions \n\n")
         for test_class in ENABLED_TESTS:
             _actual_class = test_class()
             output.write(
