d0c8d055ff2afd9e9253338b35e0a8d63267a93d
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-01 04:12:46 -0300

    Moved test creation to a separated file

--- test_runner.py
+++ test_runner.py
@@ -1,62 +1,14 @@
-from testing_class import MATCHERS, Test
+from testing_class import MATCHERS
 
-
-class TestNM1(Test):
-    _string = "a" * 499 + "b" * 50 + "a" * 499 + "b" + "a" * 499
-    _pattern = "a" * 500
-    _iterations = 1000
-
-    def run(self, f):
-        for _ in range(self._iterations - 1):
-            f(self._string, self._pattern)
-        return f(self._string, self._pattern)
-
-    def get_expected_result(self):
-        return []
-
-    def get_test_name(self):
-        return "NM-1"
-
-    def get_long_test_name(self):
-        return "No Matches Test 1"
-
-    def get_test_description(self):
-        return """
-Corre {} veces el string matcher sobre una cadena de largo {} en la cual no hay matches.
-
-Hay 3 posibles candidatos de n-1 elementos, con n={} el largo del patron.
-                """.format(self._iterations, len(self._string), len(self._pattern))
-
-
-class TestNM2(Test):
-    _string = "c" * 499 + "b" * 50 + "c" * 499 + "b" + "c" * 499
-    _pattern = "a" * 500
-    _iterations = 1000
-
-    def run(self, f):
-        for _ in range(self._iterations - 1):
-            f(self._string, self._pattern)
-        return f(self._string, self._pattern)
-
-    def get_expected_result(self):
-        return []
-
-    def get_test_name(self):
-        return "NM-2"
-
-    def get_long_test_name(self):
-        return "No Matches Test 2"
-
-    def get_test_description(self):
-        return """
-Corre {} veces el string matcher sobre una cadena de largo {} en la cual no hay matches.
-
-No hay ningun posible candidato porque no hay caracteres en comun entre el patron y el string. Patron de largo n={}.
-                """.format(self._iterations, len(self._string), len(self._pattern))
-
-
-ENABLED_TESTS = [TestNM1, TestNM2]
 TEST_RESULTS = "test_results"
+import tests_no_matches
+
+TEST_MODULES = [tests_no_matches]
+ENABLED_TESTS = []
+
+for _module in TEST_MODULES:
+    for test in _module.ENABLED_TESTS:
+        ENABLED_TESTS.append(test)
 
 
 def main():
