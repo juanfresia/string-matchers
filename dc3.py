15acad7a815547e4545515488a7e7ce73a634c4d
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-01 01:39:54 -0300

    Cleaning code. Working but needs cleanup

--- dc3.py
+++ dc3.py
@@ -11,7 +11,6 @@ def sample_suffixes_create(text):
         if len(item) < 3:
             item += (0,) * (3 - len(item))
 
-
         result[i % 3].append((item, i))
 
     return result
@@ -51,26 +50,25 @@ def generate_lookup(_list):
     return lookup
 
 
-def dcm(secuence):
-    print("Sed", secuence)
-    base_secuence = secuence
-    if type(secuence) == type(""):
-        base_secuence = tuple([ord(c) if c != "$" else 0 for c in "yabbadabbado"])
+def dcm(sequence):
+    base_sequence = sequence
+    if isinstance(sequence, str):
+        base_sequence = tuple([ord(c) if c != "$" else 0 for c in sequence])
 
-    base_secuence += (0,)
+    base_sequence += (0,)
 
-    a0, a1, a2 = sample_suffixes_create(base_secuence)
+    a0, a1, a2 = sample_suffixes_create(base_sequence)
     a12 = a1 + a2
     sorted_a12 = radix_sort(a12)
     d = generate_lookup(sorted_a12)
 
-    step_1 = tuple([d[i[0]] for i in a12])
-
+    r_sequence = tuple([d[i[0]] for i in a12])
     out = [sorted_a12]
-    if len(d) != len(sorted_a12):
-        out = dcm(step_1)
 
-    rank = [d[base_secuence[i:i + 3]] if i % 3 != 0 else "|" for i in range(len(base_secuence) - 2)] + [0, 0, 0]
+    if len(d) != len(sorted_a12):
+        out = dcm(r_sequence)
+
+    rank = [d[base_sequence[i:i + 3]] if i % 3 != 0 else "|" for i in range(len(base_sequence) - 2)] + [0, 0, 0]
 
     for i in range(1, len(out)):
         rank_index = a12[out[i]]
@@ -83,10 +81,11 @@ def dcm(secuence):
 
     sorted_a0 = radix_sort(to_sort)
 
-    merge = merge_step(rank, sorted_a0, sorted_a12, base_secuence)
+    merge = merge_step(rank, sorted_a0, sorted_a12, base_sequence)
     return merge
 
-def merge_step(ranks, sorted_b0, sorted_b12, original_secuence):
+
+def merge_step(ranks, sorted_b0, sorted_b12, original_sequence):
     i = 0
     j = 0
     merge = []
@@ -95,12 +94,12 @@ def merge_step(ranks, sorted_b0, sorted_b12, original_secuence):
         sample_index = sorted_b12[i][1]
         nsample_index = sorted_b0[j][1]
 
-        if original_secuence[sample_index] < original_secuence[nsample_index]:
+        if original_sequence[sample_index] < original_sequence[nsample_index]:
             merge.append(sample_index)
             i += 1
             continue
 
-        if original_secuence[sample_index] > original_secuence[nsample_index]:
+        if original_sequence[sample_index] > original_sequence[nsample_index]:
             merge.append(nsample_index)
             j += 1
             continue
@@ -116,12 +115,12 @@ def merge_step(ranks, sorted_b0, sorted_b12, original_secuence):
             continue
 
         # B2 case
-        if original_secuence[sample_index + 1] < original_secuence[nsample_index + 1]:
+        if original_sequence[sample_index + 1] < original_sequence[nsample_index + 1]:
             merge.append(sample_index)
             i += 1
             continue
 
-        if original_secuence[sample_index + 1] > original_secuence[nsample_index + 1]:
+        if original_sequence[sample_index + 1] > original_sequence[nsample_index + 1]:
             merge.append(nsample_index)
             j += 1
             continue
