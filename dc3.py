b4f5ab75a64d869d475ae5fe41575ef06e17e6b3
Xero-Hige <gaston.martinez.90@gmail.com>
2017-09-30 21:54:54 -0300

    Added DC3 til Step2 (hardcoded)

--- dc3.py
+++ dc3.py
@@ -13,7 +13,10 @@ def sample_suffixes_create(text):
 def radix_sort(list_to_sort):
     sorted_list = list_to_sort
 
-    for i in range(1, 4):
+    if len(sorted_list) == 0:
+        return sorted_list[:]
+
+    for i in range(1, len(sorted_list[0]) + 1):
         buckets = {}
 
         for word in sorted_list:
@@ -34,7 +37,6 @@ def generate_lookup(_list):
 
     i = 1
     for substring in _list:
-        print(substring)
         if not substring in lookup:
             lookup[substring] = i
             i += 1
@@ -42,22 +44,58 @@ def generate_lookup(_list):
     return lookup
 
 
+def DC3():
+    base_str = tuple([ord(c) for c in "yabbadabbado"] + [0, 0])
+
+    a0, a1, a2 = sample_suffixes_create(base_str)
+    sorted_sample_positions = radix_sort(a1 + a2)
+    lookup_table = generate_lookup(sorted_sample_positions)
+
+    step_1 = tuple([lookup_table[i] for i in a1 + a2])
+
+    if (len(lookup_table)) != len(a1 + a2):
+        step_1 = DC3(step_1)
+
+    sample = [n[step_1[i:i + 3]] if i % 3 != 0 else "|" for i in range(len(step_1) - 2)] + [0, 0]
+
+    to_sort = []
+    for i in range(len(a0)):
+        to_sort.append((a0[i][0], sample[i * 3 + 1]))
+
+    result = radix_sort(to_sort)
+    print(result)
+
+    base_str = tuple(base_str)
+    sample = [d[base_str[i:i + 3]] if i % 3 != 0 else "|" for i in range(len(base_str) - 2)] + ["|", 0, 0]
+    print(sample)
+
+
 if __name__ == '__main__':
-    base_str = "yabbadabbado$$"
+    base_str = tuple([c for c in "yabbadabbado$$"])
 
     a0, a1, a2 = sample_suffixes_create(base_str)
     b = radix_sort(a1 + a2)
     d = generate_lookup(b)
 
-    step_1 = [d[i] for i in a1 + a2]
+    step_1 = tuple([d[i] for i in a1 + a2]) + (0, 0)
 
-    e0, e1, e2 = sample_suffixes_create(step_1 + [0, 0])
+    e0, e1, e2 = sample_suffixes_create(step_1)
     m = radix_sort(e1 + e2)
-    n = generate_lookup(m)
+    n = generate_lookup(tuple(m))
 
-    step_2 = [n[i] for i in e1 + e2]
+    step_2 = tuple([n[i] for i in e1 + e2]) + (0, 0)
 
-    print(step_2)
+    sample = [n[step_1[i:i + 3]] if i % 3 != 0 else "|" for i in range(len(step_1) - 2)] + [0, 0]
+
+    # Non sample
+    to_sort = []
+    for i in range(len(e0)):
+        to_sort.append((e0[i][0], sample[i * 3 + 1]))
+
+    result = radix_sort(to_sort)
+
+
+
     base_str = tuple(base_str)
-    sample = [str(d[base_str[i:i + 3]]) if i % 3 != 0 else "|" for i in range(len(base_str) - 2)] + ["|", "0", "0"]
+    sample = [d[base_str[i:i + 3]] if i % 3 != 0 else "|" for i in range(len(base_str) - 2)] + ["|", 0, 0]
     print(sample)
