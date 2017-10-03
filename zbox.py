d88b9a83c3171a7c843513140a6e435a0de33460
Juan Manuel Fresia <jfresia@medallia.com>
2017-10-03 14:12:59 -0300

    Fixed some bugs on zbox algorithm
    Signed-off-by: Juan Manuel Fresia <jfresia@medallia.com>

--- zbox.py
+++ zbox.py
@@ -5,11 +5,12 @@ def test_string_matching():
     P1 = "ES"
     P2 = "HOLA"
 
-    T2 = "AAZAAA"
+    T2 = "AAZAAAA"
     P3 = "AA"
-    T3 = "ABABZABABAB"
-    P4 = "AB"
 
+
+    T3 = "c" * 499 + "b" * 50 + "c" * 499 + "b" * 453 + "c" * 499
+    P4 = "a" * 500
     print(string_matching_zcajas(T, P1))
     print(string_matching_zcajas(T, P2))
     print(string_matching_zcajas(T2, P3))
@@ -18,11 +19,14 @@ def test_string_matching():
 
 def string_matching_zcajas(texto, patron, caracter_reservado='$'):
     """Devuelve una lista con las posiciones en texto
-	donde aparece patron completo utilizando el algoritmo z."""
+    donde aparece patron completo utilizando el algoritmo z."""
 
     texto_patron = patron + caracter_reservado + texto
     zcajas = computar_zcajas(texto_patron)
-    # print("zcajas: ", zcajas)
+    #print(texto)
+    #print(patron)
+    #print(texto_patron)
+    #print("zcajas: ", zcajas)
     matches = []
     # print("largo zcajas: ", len(zcajas))
     # print("largo texto_patron: ", len(texto_patron))
@@ -30,6 +34,7 @@ def string_matching_zcajas(texto, patron, caracter_reservado='$'):
         if zcajas[x] == len(patron):
             matches.append(x - len(patron) - 1)
 
+    #print(matches)
     return matches
 
 
@@ -41,52 +46,53 @@ def computar_zcajas(texto):
     l = 0
 
     for k in range(1, len(texto)):
-        # print("k: ", k, "texto[k]: ", texto[k])
         if k > r:
             # Estoy fuera de la caja => computo caja nueva
+            # print("k: ", k)
             match_len = 0
 
             for act in range(len(texto) - k):
+                # print("act: ", act, "k:", k, texto[act], texto[act + k])
                 if texto[k + act] == texto[act]:
                     l = k
                     r = k + act
                     match_len += 1
                 else:
-                    Zs.append(match_len)
-                    # print("holuu!")
-                    act += 1
                     break
             # print("act: ", act)
             # print("len(texto) - k: ", len(texto) - k)
-            if match_len == len(texto) - k:
-                # print("holuu! 2")
-                Zs.append(match_len)
-                # print("Zs: ", match_len)
+            Zs.append(match_len)
 
         else:
             # Estoy dentro de una Zcaja
             Zprima = Zs[k - l]
+            # print("k' =", k-l, " Z' = ", Zprima)
 
             if Zprima + k >= len(texto):
+                #print("Caso 1: ", k)
                 Zprimareal = len(texto) - k
                 Zs.append(Zprimareal)
 
             elif (k + Zprima - 1) < r:
+                #print("Caso 2: ", k)
                 # La caja nueva queda contenida en la vieja.
-                # print("Zs: ", Zprima)
+                #print("Zs: ", Zprima)
                 Zs.append(Zprima)
             else:
-                match_len = Zprima
-                for act in range(k - l, len(texto) - k):
+                match_len = r - k + 1
+                # print("Caso 3: ", k)
+                for act in range(match_len, len(texto) - k - match_len + 1):
+                    # print("Inspecting: ", k, act, k+act)
+                    # print("act: ", act, "k:", k, texto[act], texto[act + k])
                     if texto[k + act] == texto[act]:
                         l = k
                         r = k + act
                         match_len += 1
                     else:
-                        Zs.append(match_len)
+                        # Zs.append(match_len)
                         break
-                if match_len == len(texto) - k:
-                    Zs.append(match_len)
+                #if match_len == len(texto) - k:
+                Zs.append(match_len)
                     # print("Zs: ", match_len)
                     # print("Zs: ", Zs)
     # print(texto)
