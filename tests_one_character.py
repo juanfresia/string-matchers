44e9c1bd78fc3028776eeb9d1980f1135cca8b12
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-07 04:23:43 -0300

    Added only one character tests

---
+++ tests_one_character.py
@@ -0,0 +1,58 @@
+from testing_class import Test
+
+
+class TestOC1(Test):
+    _pattern = "a" * 500
+    _string = "a" * 2000
+    _iterations = 1000
+
+    def run(self, f):
+        for _ in range(self._iterations - 1):
+            f(self._string, self._pattern)
+        return f(self._string, self._pattern)
+
+    def get_expected_result(self):
+        return [x for x in range(len(self._string) - len(self._pattern) + 1)]
+
+    def get_test_name(self):
+        return "OC-1"
+
+    def get_long_test_name(self):
+        return "One Character Test 1"
+
+    def get_test_description(self):
+        return """
+Corre {} veces el string matcher sobre una cadena de largo {} en la cual hay solo un caracter.
+
+Hay largo de la cadena menos n matches, siendo n={} el largo del patron.
+                """.format(self._iterations, len(self._string), len(self._pattern))
+
+
+class TestOC2(Test):
+    _pattern = "a"
+    _string = "a" * 2000
+    _iterations = 1000
+
+    def run(self, f):
+        for _ in range(self._iterations - 1):
+            f(self._string, self._pattern)
+        return f(self._string, self._pattern)
+
+    def get_expected_result(self):
+        return [x for x in range(len(self._string) - len(self._pattern) + 1)]
+
+    def get_test_name(self):
+        return "OC-2"
+
+    def get_long_test_name(self):
+        return "One Character Test 2"
+
+    def get_test_description(self):
+        return """
+Corre {} veces el string matcher sobre una cadena de largo {} en la cual hay solo un caracter.
+
+Hay largo de la cadena menos n matches, siendo n={} el largo del patron (patron corto).
+                """.format(self._iterations, len(self._string), len(self._pattern))
+
+
+ENABLED_TESTS = [TestOC1, TestOC2]
