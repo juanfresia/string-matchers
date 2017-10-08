a144dd3ed4a0302fe47177b9123406f0d143b566
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-07 22:12:08 -0300

    Fixed result check at multipattern class

--- testing_multi_class.py
+++ testing_multi_class.py
@@ -23,13 +23,15 @@ class TestMultimatch(Test):
         result_string = "|[{}](#{})|".format(self.get_test_name(), self.get_long_test_name().replace(" ", "-").lower())
 
         for tag in sorted(MATCHERS.keys()):
-            result, run_time = self.cron_test(MATCHERS[tag])
-            if result:
-                result.sort()
-            status = "OK" if result == self.get_expected_result() else "ERROR"
+            results, run_time = self.cron_test(MATCHERS[tag])
+            if results:
+                for result in results:
+                    result.sort()
+
+            status = "OK" if results == self.get_expected_result() else "ERROR"
 
             if status != "OK" and VERBOSE:
-                print("Error: <{2}> expected:{1}, got:{0}".format(result, self.get_expected_result(), tag))
+                print("Error: <{2}> expected:{1}, got:{0}".format(results, self.get_expected_result(), tag))
 
             result_string += "{:03.6f} - ({})|".format(run_time, status)
 
