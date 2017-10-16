89afba86a3aa29fc376f6eca0a14ce66e019f72c
Xero-Hige <gaston.martinez.90@gmail.com>
2017-10-16 17:15:55 -0300

    Added multiple matches over big strings

--- tests_one_character.py
+++ tests_one_character.py
@@ -175,4 +175,85 @@ Hay largo de la cadena menos n matches, siendo n={} el largo del patron (patron 
                 """.format(self._iterations, len(self._string), len(self._pattern))
 
 
-ENABLED_TESTS = [TestOC1, TestOC2, TestOC3, TestOC4, TestOC5, TestOC6]
+class TestOC7(Test):
+    _pattern = "a" * 500
+    _string = "a" * 10000
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self, f):
+        return f(self._string, self._pattern)
+
+    def get_expected_result(self):
+        return [x for x in range(len(self._string) - len(self._pattern) + 1)]
+
+    def get_test_name(self):
+        return "OC-7"
+
+    def get_long_test_name(self):
+        return "One Character Test 7"
+
+    def get_test_description(self):
+        return """
+Corre {} veces el string matcher sobre una cadena de largo {} en la cual hay solo un caracter.
+
+Hay largo de la cadena menos n matches, siendo n={} el largo del patron (patron corto).
+                """.format(self._iterations, len(self._string), len(self._pattern))
+
+
+class TestOC8(Test):
+    _pattern = "a" * 500
+    _string = "a" * 100000
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self, f):
+        return f(self._string, self._pattern)
+
+    def get_expected_result(self):
+        return [x for x in range(len(self._string) - len(self._pattern) + 1)]
+
+    def get_test_name(self):
+        return "OC-8"
+
+    def get_long_test_name(self):
+        return "One Character Test 8"
+
+    def get_test_description(self):
+        return """
+Corre {} veces el string matcher sobre una cadena de largo {} en la cual hay solo un caracter.
+
+Hay largo de la cadena menos n matches, siendo n={} el largo del patron (patron corto de largo par).
+                """.format(self._iterations, len(self._string), len(self._pattern))
+
+
+class TestOC9(Test):
+    _pattern = "a" * 500
+    _string = "a" * 1000000
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self, f):
+        return f(self._string, self._pattern)
+
+    def get_expected_result(self):
+        return [x for x in range(len(self._string) - len(self._pattern) + 1)]
+
+    def get_test_name(self):
+        return "OC-9"
+
+    def get_long_test_name(self):
+        return "One Character Test 9"
+
+    def get_test_description(self):
+        return """
+Corre {} veces el string matcher sobre una cadena de largo {} en la cual hay solo un caracter.
+
+Hay largo de la cadena menos n matches, siendo n={} el largo del patron (patron corto de largo impar).
+                """.format(self._iterations, len(self._string), len(self._pattern))
+
+
+ENABLED_TESTS = [TestOC1, TestOC2, TestOC3, TestOC4, TestOC5, TestOC6, TestOC7, TestOC8, TestOC9]
